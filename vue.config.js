c01e515f9f19b755943b61b11f47d4c76f044667
cynthia0329 <1440731632@qq.com>
2020-04-15 17:53:16 +0800

    统一引入scss文件

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 // 配置参考：https://cli.vuejs.org/zh/config/
 
 module.exports = {
-  // 选项...
+  css: {
+    // css预设器配置项
+    loaderOptions: {
+      //设置css中引用文件的路径，引入通用使用的scss文件
+      sass: {
+        data: `@import "@/styles/index.scss";`
+      }
+    }
+  }
 }
